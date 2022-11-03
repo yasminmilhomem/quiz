4588f7ebe5c93da93c5737e073c7cfbb42c1a20a
yasminmilhomem <yasminmilhomem1517@gmail.com>
2022-11-03 20:31:03 -0300

    comentario

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -8,15 +8,23 @@ import './GameOver.css'
 
 const GameOver = () => {
     const [quizState, dispatch] = useContext(QuizContext);
-
+    function verificaPontuacao(){
+      if(quizState.score === 0){
+        return <p>Burrao, Leigo!*</p>
+      }else{
+        return <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas!</p>
+      }
+    }
   return (
-    <div id='gameover'>
+    <>
+      <div id='gameover'>
         <h2>Fim de Jogo!</h2>
         <p id='scorep'>Pontuação: {quizState.score}</p>
-        <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas!</p>
+        { verificaPontuacao() }
         <img src={WellDone} alt="Fim do Quiz!" />
         <button onClick={() => dispatch({ type: "NEW_GAME" })}>Reiniciar</button>
-    </div>
+      </div>
+    </>
   )
 }
 
